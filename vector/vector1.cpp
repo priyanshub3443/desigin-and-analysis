d57a2f3062bb27e1dded2dd3b443266ffa384bf7
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-03-19 23:38:06 +0530

    Update vector1.cpp

--- vector/vector1.cpp
+++ vector/vector1.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+int main() {
     vector<int> vec = {12, 34, 56, 78, 56, 89};
-    cout<<vec.size;
+
+    // Printing the size of the vector
+    cout << "Size of the vector: " << vec.size() << endl;
+
+    // Printing the elements of the vector
+    cout << "Elements of the vector: ";
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
